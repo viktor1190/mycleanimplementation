cf2b040e4ad9ef88b5b1a24da6d9bce6ca6839e9
Victor J. Valencia Gomez <cytrux@gmail.com>
2018-06-28 01:10:39 -0500

    Implementing the UnBookmark Project use case

--- Domain/src/main/java/com/android/devmonkey/domain/interactor/bookmark/UnbookmarkProjectUsecase.kt
+++ Domain/src/main/java/com/android/devmonkey/domain/interactor/bookmark/UnbookmarkProjectUsecase.kt
@@ -6,19 +6,19 @@ import com.android.devmonkey.domain.repository.ProjectRepository
 import io.reactivex.Completable
 import javax.inject.Inject
 
-class BookmarkProjectUseCase @Inject constructor(
+class UnbookmarkProjectUsecase @Inject constructor(
         private val projectRepository: ProjectRepository,
         postExecutionThread: PostExecutionThread)
-    : CompletableUseCase<BookmarkProjectUseCase.Params> (postExecutionThread) {
+    : CompletableUseCase<UnbookmarkProjectUsecase.Params>(postExecutionThread) {
 
     override fun buildUseCaseCompletable(params: Params?): Completable {
         if (params == null) throw IllegalArgumentException("Params can't be null")
-        return projectRepository.bookmarkProject(params.projectId)
+        return projectRepository.unbookmarkProject(params.projectId)
     }
 
     data class Params constructor(val projectId: String) {
         companion object {
-            fun forProject(projectId: String): Params = Params(projectId)
+            fun forProject(projectId: String) = Params(projectId)
         }
     }
 }
