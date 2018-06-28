d2890fddfb5aed8d1ab97e49601244877c2a57e9
Victor J. Valencia Gomez <cytrux@gmail.com>
2018-06-28 00:46:45 -0500

    Implementing the Get Projects use case

---
+++ Domain/src/main/java/com/android/devmonkey/domain/interactor/browse/GetProjectsUseCase.kt
@@ -0,0 +1,18 @@
+package com.android.devmonkey.domain.interactor.browse
+
+import com.android.devmonkey.domain.executor.PostExecutionThread
+import com.android.devmonkey.domain.interactor.ObservableUseCase
+import com.android.devmonkey.domain.model.Project
+import com.android.devmonkey.domain.repository.ProjectRepository
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class GetProjectsUseCase @Inject constructor(
+        private val projectRepository: ProjectRepository,
+        postExecutionThread: PostExecutionThread)
+    : ObservableUseCase<List<Project>, Nothing>(postExecutionThread) {
+
+    override fun buildUseCase(params: Nothing?): Observable<List<Project>> {
+        return projectRepository.getProjects()
+    }
+}
